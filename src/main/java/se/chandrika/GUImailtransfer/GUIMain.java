9d9d43b0b9fec8040dcea6c7b022cf53f86f56cf
chandrika-karri <chandrika.karri@gmail.com>
2024-10-15 09:13:17 +0200

    updated java files

--- src/main/java/se/chandrika/GUImailtransfer/GUIMain.java
+++ src/main/java/se/chandrika/GUImailtransfer/GUIMain.java
@@ -7,23 +7,23 @@ public class GUIMain {
 
     public static void main(String[] args) {
 
-        Email email = new Email("chandrika.kovvuri@gmail.com","enaw encs tdmb pddv");
+        Email email = new Email("chandrika.kovvuri@gmail.com", "enaw encs tdmb pddv");
         SendMail sendmail = new SendMail(email);
 
-        Mail mail = new Mail("chandrika.kovvuri@gmail.com", "hello", "GUI mail", null);
+
         GUI gui = new GUI();
-        mail = gui.showEmailPanel();
+        Mail mail = gui.showEmailPanel();
 
         if (mail != null) {
             sendmail.sendEmail(mail.getTo(), mail.getSubject(), mail.getText());
 
-            System.out.println(mail.getTo() + ", " + mail.getSubject() + ", " + mail.getText());
         }
 
         while (true) {
             mail = gui.showEmailPanel();
             if (mail != null) {
                 sendmail.sendEmail(mail.getTo(), mail.getSubject(), mail.getText());
+
             } else {
 
                 break;
